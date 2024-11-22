84c94792256de5e0b4119acb6399387ac7cbf6e4
KirtanDC <kirtan.patel5@dcmail.ca>
2024-11-22 18:26:27 -0500

    removed additional logo method

--- In_Class_3/frmMain.Designer.cs
+++ In_Class_3/frmMain.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.dgvPlayers = new System.Windows.Forms.DataGridView();
             this.fullnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -46,19 +46,11 @@
             this.dataTable1TableAdapter = new In_Class_3.SportleaguesDataSet1TableAdapters.DataTable1TableAdapter();
             this.lblTeamName = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.panBosna = new System.Windows.Forms.Panel();
-            this.panRangers = new System.Windows.Forms.Panel();
-            this.panStaffords = new System.Windows.Forms.Panel();
-            this.panStrikers = new System.Windows.Forms.Panel();
-            this.panAurora = new System.Windows.Forms.Panel();
-            this.panKickers = new System.Windows.Forms.Panel();
-            this.panEagles = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPlayers)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sportleaguesDataSet11)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.teamsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sportleaguesDataSet1)).BeginInit();
-            this.panRangers.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvPlayers
@@ -67,14 +59,14 @@
             this.dgvPlayers.AllowUserToDeleteRows = false;
             this.dgvPlayers.AutoGenerateColumns = false;
             this.dgvPlayers.BackgroundColor = System.Drawing.Color.White;
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvPlayers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPlayers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvPlayers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPlayers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.fullnameDataGridViewTextBoxColumn,
@@ -82,14 +74,14 @@
             this.jerseynumberDataGridViewTextBoxColumn,
             this.isactiveDataGridViewTextBoxColumn});
             this.dgvPlayers.DataSource = this.dataTable1BindingSource;
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvPlayers.DefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPlayers.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvPlayers.GridColor = System.Drawing.SystemColors.ButtonFace;
             this.dgvPlayers.Location = new System.Drawing.Point(282, 116);
             this.dgvPlayers.Name = "dgvPlayers";
@@ -203,72 +195,6 @@
             this.lblTitle.Text = "Sportleagues Team Information Table";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panBosna
-            // 
-            this.panBosna.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panBosna.BackgroundImage")));
-            this.panBosna.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panBosna.Location = new System.Drawing.Point(41, 82);
-            this.panBosna.Name = "panBosna";
-            this.panBosna.Size = new System.Drawing.Size(195, 156);
-            this.panBosna.TabIndex = 6;
-            // 
-            // panRangers
-            // 
-            this.panRangers.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panRangers.BackgroundImage")));
-            this.panRangers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panRangers.Controls.Add(this.panStaffords);
-            this.panRangers.Controls.Add(this.panStrikers);
-            this.panRangers.Controls.Add(this.panAurora);
-            this.panRangers.Location = new System.Drawing.Point(44, 79);
-            this.panRangers.Name = "panRangers";
-            this.panRangers.Size = new System.Drawing.Size(195, 156);
-            this.panRangers.TabIndex = 7;
-            // 
-            // panStaffords
-            // 
-            this.panStaffords.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panStaffords.BackgroundImage")));
-            this.panStaffords.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panStaffords.Location = new System.Drawing.Point(0, 3);
-            this.panStaffords.Name = "panStaffords";
-            this.panStaffords.Size = new System.Drawing.Size(195, 156);
-            this.panStaffords.TabIndex = 11;
-            // 
-            // panStrikers
-            // 
-            this.panStrikers.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panStrikers.BackgroundImage")));
-            this.panStrikers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panStrikers.Location = new System.Drawing.Point(0, 6);
-            this.panStrikers.Name = "panStrikers";
-            this.panStrikers.Size = new System.Drawing.Size(195, 156);
-            this.panStrikers.TabIndex = 9;
-            // 
-            // panAurora
-            // 
-            this.panAurora.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panAurora.BackgroundImage")));
-            this.panAurora.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panAurora.Location = new System.Drawing.Point(3, 6);
-            this.panAurora.Name = "panAurora";
-            this.panAurora.Size = new System.Drawing.Size(195, 156);
-            this.panAurora.TabIndex = 8;
-            // 
-            // panKickers
-            // 
-            this.panKickers.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panKickers.BackgroundImage")));
-            this.panKickers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panKickers.Location = new System.Drawing.Point(41, 82);
-            this.panKickers.Name = "panKickers";
-            this.panKickers.Size = new System.Drawing.Size(195, 156);
-            this.panKickers.TabIndex = 9;
-            // 
-            // panEagles
-            // 
-            this.panEagles.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panEagles.BackgroundImage")));
-            this.panEagles.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panEagles.Location = new System.Drawing.Point(41, 88);
-            this.panEagles.Name = "panEagles";
-            this.panEagles.Size = new System.Drawing.Size(195, 156);
-            this.panEagles.TabIndex = 10;
-            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -277,10 +203,6 @@
             this.BackgroundImage = global::In_Class_3.Properties.Resources.field;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1074, 550);
-            this.Controls.Add(this.panEagles);
-            this.Controls.Add(this.panRangers);
-            this.Controls.Add(this.panKickers);
-            this.Controls.Add(this.panBosna);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblTeamName);
             this.Controls.Add(this.cmbTeams);
@@ -295,7 +217,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.sportleaguesDataSet11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.teamsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.sportleaguesDataSet1)).EndInit();
-            this.panRangers.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -316,13 +237,6 @@
         private System.Windows.Forms.Label lblTeamName;
         private System.Windows.Forms.ComboBox cmbTeams;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Panel panBosna;
-        private System.Windows.Forms.Panel panRangers;
-        private System.Windows.Forms.Panel panAurora;
-        private System.Windows.Forms.Panel panKickers;
-        private System.Windows.Forms.Panel panStrikers;
-        private System.Windows.Forms.Panel panEagles;
-        private System.Windows.Forms.Panel panStaffords;
     }
 }
 
--- In_Class_3/frmMain.cs
+++ In_Class_3/frmMain.cs
@@ -30,7 +30,7 @@ namespace In_Class_3
         {
             InitializeComponent();
         }
-        
+
         private Dictionary<int, Panel> teamPanels;
         #region Form Load Event
 
@@ -41,26 +41,10 @@ namespace In_Class_3
         /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
-            ResetForm();
             // TODO: This line of code loads data into the 'sportleaguesDataSet11.DataTable1' table. You can move, or remove it, as needed.
             this.dataTable1TableAdapter.Fill(this.sportleaguesDataSet11.DataTable1);
             // TODO: This line of code loads data into the 'sportleaguesDataSet1.teams' table. You can move, or remove it, as needed.
             this.teamsTableAdapter.Fill(this.sportleaguesDataSet1.teams);
-
-            teamPanels = new Dictionary<int, Panel>
-            {
-                { 210, panRangers },
-                { 211, panBosna },
-                { 212, panAurora },
-                { 214, panKickers },
-                { 215, panStrikers },
-                { 216, panEagles },
-                { 218, panStaffords },
-            };
-            foreach (var panel in teamPanels.Values)
-            {
-                panel.Visible = false;
-            }
         }
         #endregion
 
@@ -78,52 +62,15 @@ namespace In_Class_3
                 try
                 {
                     int teamvalue = int.Parse(cmbTeams.SelectedValue.ToString());
-                    ShowTeamLogo(teamvalue);
                     this.dataTable1TableAdapter.fillByTeam(this.sportleaguesDataSet11.DataTable1, teamvalue);
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-                
-            }
-        }
 
-        #endregion
-
-        #region Methods
-        /// <summary>
-        /// Method to Reset the form on each load event
-        /// </summary>
-        private void ResetForm()
-        {
-            panRangers.Visible = false; panBosna.Visible = false; panAurora.Visible = false;
-            panEagles.Visible = false; panKickers.Visible = false; panStaffords.Visible = false;
-            panStrikers.Visible = false;
-        }
-
-        /// <summary>
-        /// Method to Show team logo of selected team respectively
-        /// </summary>
-        /// <param name="teamId"></param>
-        private void ShowTeamLogo(int teamId)
-        {
-            // Hide all panels
-            foreach (var panel in teamPanels.Values)
-            {
-                panel.Visible = false;
-            }
-
-            // Show the correct panel if it exists in the dictionary
-            if (teamPanels.ContainsKey(teamId))
-            {
-                teamPanels[teamId].Visible = true;
-            }
-            else
-            {
-                MessageBox.Show("No logo available for this team.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
         #endregion
-    }
+    }   
 }
